e7a313a4ef4df76c3a2a4dbba58e8773eb2044d5
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2019-01-23 21:35:30 +0100

    added link do CV

--- portfolio/src/components/subcomponents/HeaderPart/AboutMe.jsx
+++ portfolio/src/components/subcomponents/HeaderPart/AboutMe.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import download from '../../../img/download.png'
 
 const AboutMe = props => (
     <div>
         <h3>{props.content.header}</h3>
         <p>{props.content.paragraphOne}</p>
         <p>{props.content.paragraphTwo}</p>
+        <div>
+            <a href="https://www.dropbox.com/s/805acixlm2lo5im/CV%20Mateusz%20Grzymowicz.pdf?dl=1">
+                <img src={download} alt="pobierz CV"/>    
+            </a>
+            </div>
     </div>
 )
 
