d607e71e32a6d4728852fc5419740dd06f69df96
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2019-01-24 11:33:22 +0100

    fixed description of projects

--- portfolio/src/components/subcomponents/ProjectPart/ListOfProjects.jsx
+++ portfolio/src/components/subcomponents/ProjectPart/ListOfProjects.jsx
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
-import {Row, Col, Image} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
 import WOW from 'wowjs';
 
 class ListOfProjects extends Component {
     constructor(props) {
-        super (props)
+        super(props)
         this.state = {
             display: "none",
         }
@@ -16,24 +16,26 @@ class ListOfProjects extends Component {
         }).init();
     }
 
-    showDescription(){
-        this.setState({display: "flex"})
+    showDescription() {
+        this.setState({ display: "flex" })
     }
-    hideDescription(){
-        this.setState({display: "none"})
+    hideDescription() {
+        this.setState({ display: "none" })
     }
 
     render() {
         return (
             <Row className="show-grid text-center projects-list wow zoomInLeft" data-wow-duration="2s">
                 <Col xs={1} sm={1} className="projects"
-                     onMouseOver={this.showDescription.bind(this)}
-                     onMouseOut={this.hideDescription.bind(this)}>
+                    onMouseOver={this.showDescription.bind(this)}
+                    onMouseOut={this.hideDescription.bind(this)}>
                     <h1>{this.props.text}</h1>
-                    <a href={this.props.website}>
-                        <Image src={this.props.photo} alt="project" thumbnail responsive/>
-                    </a>
-                    <span className="description-project" style={{display: this.state.display}}>{this.props.technologies}</span>
+                    <div>
+                        <a href={this.props.website}>
+                            <Image src={this.props.photo} alt="project" thumbnail responsive />
+                        </a>
+                        <span className="description-project" style={{ display: this.state.display }}>{this.props.technologies}</span>
+                    </div>
                 </Col>
             </Row>
         );
